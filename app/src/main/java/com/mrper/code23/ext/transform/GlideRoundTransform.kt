4486f20f47edcd353b8651b7a90d5b3fb35a27e9
Cosecant <yugecosecant@foxmail.com>
2016-04-20 17:35:00 +0800

    (changed)优化用户体验，类型添加全部类型

--- app/src/main/java/com/mrper/code23/ext/transform/GlideRoundTransform.kt
+++ app/src/main/java/com/mrper/code23/ext/transform/GlideRoundTransform.kt
@@ -10,18 +10,16 @@ import com.bumptech.glide.load.resource.bitmap.BitmapTransformation
  * Created by admin on 2016/4/18.
  */
 class GlideRoundTransform @JvmOverloads constructor(context: Context, dp: Int = 20) : BitmapTransformation(context) {
+
     private var radius = 0f
+
     init {
         this.radius = Resources.getSystem().displayMetrics.density * dp
     }
 
-    override fun transform(pool: BitmapPool, toTransform: Bitmap, outWidth: Int, outHeight: Int): Bitmap? {
-        return roundCrop(pool, toTransform)
-    }
+    override fun transform(pool: BitmapPool, toTransform: Bitmap, outWidth: Int, outHeight: Int): Bitmap? = roundCrop(pool, toTransform)
 
-    override fun getId(): String {
-        return javaClass.name + Math.round(radius)
-    }
+    override fun getId(): String = javaClass.name + Math.round(radius)
 
     private fun roundCrop(pool: BitmapPool, source: Bitmap?): Bitmap? {
         if (source == null) return null
